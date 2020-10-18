6c39ce8ef24d80f01ef356224378219fcfbca25d
A Brandon <64425431+a-brandon-c-v@users.noreply.github.com>
2020-10-18 02:20:25 -0400

    Add files via upload

--- r3_a11.ino
+++ r3_a11.ino
@@ -5,8 +5,8 @@ const int MotorPin2=6;
 const int MotorPin3=10;
 const int MotorPin4=9;
 //Switch
-const int PinDSwitch1= 13;
-const int PinDSwitch2= 12;
+const int PinDSwitch1= 12;
+const int PinDSwitch2= 11;
 const int PinDSwitch3= 4;
 const int PinDSwitch4= 3;
 
@@ -24,83 +24,63 @@ void setup()
   pinMode(MotorPin2, OUTPUT);
   pinMode(MotorPin3, OUTPUT);
   pinMode(MotorPin4, OUTPUT);
+  Serial.begin(9600);
   
 }
 
 void loop()
 {
+  Serial.println(digitalRead(PinDSwitch1));
+  Serial.println(digitalRead(PinDSwitch2));
+  Serial.println(digitalRead(PinDSwitch3));
+  Serial.println(digitalRead(PinDSwitch4));
   
-  
+
 if (digitalRead(PinDSwitch1)== LOW && digitalRead(PinDSwitch2)== LOW && digitalRead(PinDSwitch3)== LOW && digitalRead(PinDSwitch4)== LOW)
  {
+  Serial.println("Forwards");
  //Turn Motor1 and Motor 2 FWD (CWX2) for 5 sec
   digitalWrite(MotorPin1, HIGH); //M1 (LEFT)
   digitalWrite(MotorPin2, LOW);  
   digitalWrite(MotorPin3, HIGH); //M2 (RIGHT)
   digitalWrite(MotorPin4, LOW);
-  delay (5000);
- //Turn Motor1 and Motor 2 FULL STOP for 5 sec
-  digitalWrite(MotorPin1, LOW);
-  digitalWrite(MotorPin2, LOW);  
-  digitalWrite(MotorPin3, LOW);
-  digitalWrite(MotorPin4, LOW);
-  delay (5000); } 
+} 
 
 else if (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== LOW && digitalRead(PinDSwitch3)== LOW && digitalRead(PinDSwitch4)== LOW){
+  Serial.println("Backwards");
   //Turn Motor1 and Motor 2 BACKWARDS (CCWX2) for 5 sec
   digitalWrite(MotorPin1, LOW);
   digitalWrite(MotorPin2, HIGH);  
   digitalWrite(MotorPin3, LOW);
   digitalWrite(MotorPin4, HIGH);
-  delay (5000);
-   //Turn Motor1 and Motor 2 FULL STOP for 5 sec
-  digitalWrite(MotorPin1, LOW);
-  digitalWrite(MotorPin2, LOW);  
-  digitalWrite(MotorPin3, LOW);
-  digitalWrite(MotorPin4, LOW);
-  delay (5000); }
-  
+}
 else if (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== HIGH && digitalRead(PinDSwitch3)== LOW && digitalRead(PinDSwitch4)== LOW){
-   
+   Serial.println("left");
    //Turn Motor1 and Motor 2 LEFT for 5 (CW X CCW)sec
   digitalWrite(MotorPin1, LOW);//M1 LEFT
   digitalWrite(MotorPin2, HIGH);  
   digitalWrite(MotorPin3, HIGH);//M2 RIGHT
   digitalWrite(MotorPin4, LOW);
-  delay (5000);
-   //Turn Motor1 and Motor 2 FULL STOP for 5 sec
-  digitalWrite(MotorPin1, LOW);
-  digitalWrite(MotorPin2, LOW);  
-  digitalWrite(MotorPin3, LOW);
-  digitalWrite(MotorPin4, LOW);
-  delay (5000);}
+}
   
 else if (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== HIGH && digitalRead(PinDSwitch3)== HIGH && digitalRead(PinDSwitch4)== LOW){
+  Serial.println("right");
  //Turn Motor1 and Motor 2 RIGHT (CCW X CW) for 5 sec 
   digitalWrite(MotorPin1, HIGH);
   digitalWrite(MotorPin2, LOW);  
   digitalWrite(MotorPin3, LOW);
   digitalWrite(MotorPin4, HIGH);
-  delay (5000);
-   
- 
-
-   //Turn Motor1 and Motor 2 FULL STOP for 5 sec
-  digitalWrite(MotorPin1, LOW);
-  digitalWrite(MotorPin2, LOW);  
-  digitalWrite(MotorPin3, LOW);
-  digitalWrite(MotorPin4, LOW);
-  delay (5000);}
-  
-else (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== HIGH && digitalRead(PinDSwitch3)== HIGH && digitalRead(PinDSwitch4)== HIGH);{
-   {
-  digitalWrite(MotorPin1, LOW);
-  digitalWrite(MotorPin2, LOW);  
-  digitalWrite(MotorPin3, LOW);
-  digitalWrite(MotorPin4, LOW);
-  delay (5000);}
-  
 }
+  
+else if (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== HIGH && digitalRead(PinDSwitch3)== HIGH && digitalRead(PinDSwitch4)== HIGH){
+ 
+  Serial.println("Stop");
+  digitalWrite(MotorPin1, LOW);
+  digitalWrite(MotorPin2, LOW);  
+  digitalWrite(MotorPin3, LOW);
+  digitalWrite(MotorPin4, LOW);
+ }
+  
   
 }
   
@@ -116,5 +96,9 @@ else (digitalRead(PinDSwitch1)== HIGH && digitalRead(PinDSwitch2)== HIGH && digi
   //positive rpm= cw= both motors=moving fwd
   //negative rpm =ccw= both motors=moving bckwd
   //oposite rpm=cw vs ccw; ccw vs cw =left or right move
+  
+
+  
+  
  
 
